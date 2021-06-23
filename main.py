c2d85d5031e1646aa8f7725dc5efbeacbd3e4385
World For Dev <76597998+ayoubechchetyouy@users.noreply.github.com>
2021-06-23 14:22:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
+import time
+product = input('Enter product name')
 web = webdriver.Chrome()
 web.set_window_position(2000,550)
 web.get("https://www.amazon.com/")
-product = input('Enter product name')
 y = web.find_element_by_id("twotabsearchtextbox")
 y.send_keys(product)
 w = web.find_element_by_id("nav-search-submit-button")
 w.click()
 ip = web.find_element_by_class_name("s-image")
 ip.click()
-cart = web.find_element_by_id("add-to-cart-button")
-cart.click()
+try:
+    cart = web.find_element_by_id("add-to-cart-button")
+    cart.click()
+except:
+    while True:
+        time.sleep(2)
+        web.refresh()
