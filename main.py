8c52a5614c8b5ef8293562fb0c12a77ef872285c
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-06-23 14:13:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+web = webdriver.Chrome()
+web.set_window_position(2000,550)
+web.get("https://www.amazon.com/")
+product = input('Enter product name')
+y = web.find_element_by_id("twotabsearchtextbox")
+y.send_keys(product)
+w = web.find_element_by_id("nav-search-submit-button")
+w.click()
+ip = web.find_element_by_class_name("s-image")
+ip.click()
+cart = web.find_element_by_id("add-to-cart-button")
+cart.click()
